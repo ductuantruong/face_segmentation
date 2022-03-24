7cabb7a25e0968c42ec222b244d8b3c722032b7c
ductuantruong <ductuanpy99@gmail.com>
2022-03-24 17:08:53 +0800

    change config

--- parameter.py
+++ parameter.py
@@ -13,11 +13,11 @@ def get_parameters():
     parser.add_argument('--version', type=str, default='parsenet')
 
     # Training setting
-    parser.add_argument('--total_epoch', type=int, default=18, help='how many epochs')
-    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--total_epoch', type=int, default=25, help='how many epochs')
+    parser.add_argument('--batch_size', type=int, default=8)
     parser.add_argument('--num_workers', type=int, default=2)
-    parser.add_argument('--g_lr', type=float, default=0.0002)
-    parser.add_argument('--lr_decay', type=float, default=.95)
+    parser.add_argument('--g_lr', type=float, default=0.00005)
+    parser.add_argument('--lr_decay', type=float, default=.99)
     parser.add_argument('--beta1', type=float, default=0.9)
     parser.add_argument('--beta2', type=float, default=0.999)
 
